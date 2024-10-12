335042752cf3139c5da1acda239cb728ccbdb692
Eniola Benjamin-Ade <2430301+benjaminjnr@users.noreply.github.com>
2024-10-12 09:08:30 +0100

    Second commit

---
+++ lists.py
@@ -0,0 +1,63 @@
+
+lucky_numbers = [4, 23, 16, 42, 8 , 15]
+
+friends = ["John", "Pat", "Gary", "Michael", "Tom", "Harry"]
+
+print(friends[0])
+
+print(friends[2])
+
+print(friends[-1])
+
+print(friends[2:])
+
+print(friends[1:3])
+
+friends[2] = "Mike"
+
+print(friends[2])
+
+print(friends)
+
+friends.extend(["Creed", "Toby"])
+
+print(friends)
+
+friends.append("Oscar")
+
+print(friends)
+
+friends.insert(1, "Kelly")
+
+print(friends)
+
+friends.remove("Kelly")
+
+print(friends)
+
+friends.pop()
+
+print(friends)
+
+print(friends.index("Tom"))
+
+print(friends.count("Tom"))
+
+friends.sort()
+
+print(friends)
+
+print(lucky_numbers)
+
+lucky_numbers.sort()
+
+print(lucky_numbers)
+
+lucky_numbers.reverse()
+
+print(lucky_numbers)
+
+friends.clear()
+
+print(friends)
+
---
+++ tuples.py
@@ -0,0 +1,8 @@
+
+# Tuples are immutable, meaning you can't change the values in them
+coordinates = (4, 5)
+
+print(coordinates)
+
+print(coordinates[1])
+
